f7b7f4576d8dfe944c6df6bcdf63cbc49d7026f3
Anusha <anushabt@gmail.com>
2021-04-23 23:17:39 -0500

    employee landing page and edit form

---
+++ src/main/java/org/launchcode/employeemanagementsystem/models/Employee.java
@@ -0,0 +1,88 @@
+package org.launchcode.employeemanagementsystem.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private int employeeId;
+
+    @NotBlank(message = "First Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+    private String lastName;
+
+    @Email(message="Invalid Email. Try again!")
+    private String contactEmail;
+
+    @Size(max = 500, message = "Address too long!")
+    private String address;
+
+    @Size(min=10, max=10,message="Phone number must be 10 digits")
+    private int phoneNumber;
+
+    public Employee() {}
+
+    public Employee(String firstName, String lastName, String contactEmail, String address, int phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactEmail = contactEmail;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+    //getters and setters
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
