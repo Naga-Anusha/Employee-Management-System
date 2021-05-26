1d3cd41be4b0e0b1eda8ebeb4bee43e26db19ba3
Anusha <anushabt@gmail.com>
2021-05-25 23:01:00 -0500

    Entity class for assignments and relation to user

---
+++ src/main/java/org/launchcode/employeemanagementsystem/models/Assignments.java
@@ -0,0 +1,35 @@
+package org.launchcode.employeemanagementsystem.models;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Assignments extends AbstractEntity{
+
+    private int noOfAssignments;
+
+    @OneToOne
+    User user;
+
+    public Assignments() { }
+
+    public Assignments(int noOfAssignments){
+        this.noOfAssignments = noOfAssignments;
+    }
+
+    public int getNoOfAssignments() {
+        return noOfAssignments;
+    }
+
+    public void setNoOfAssignments(int noOfAssignments) {
+        this.noOfAssignments = noOfAssignments;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
